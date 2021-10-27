1b7e9ee60a5a0e810055295d967ba309e1c1d5dd
Federico morrone <contact@morrone.dev>
2021-10-27 23:58:33 +0200

    Added try methods

--- bento/src/decode/object.rs
+++ bento/src/decode/object.rs
@@ -1,4 +1,7 @@
-use super::decoder::{DictionaryDecoder, ListDecoder};
+use super::{
+    decoder::{DictionaryDecoder, ListDecoder},
+    DecodingError,
+};
 
 pub enum Object<'obj, 'de: 'obj> {
     ByteString(&'de [u8]),
@@ -15,10 +18,24 @@ impl<'obj, 'de: 'obj> Object<'obj, 'de> {
         }
     }
 
+    pub const fn try_byte_string(self) -> Result<&'de [u8], DecodingError> {
+        match self {
+            Object::ByteString(byte_string) => Ok(byte_string),
+            _ => Err(DecodingError::Unknown),
+        }
+    }
+
     pub const fn integer(self) -> Option<&'de [u8]> {
         match self {
             Object::Integer(integer) => Some(integer),
             _ => None,
+        }
+    }
+
+    pub const fn try_integer(self) -> Result<&'de [u8], DecodingError> {
+        match self {
+            Object::Integer(integer) => Ok(integer),
+            _ => Err(DecodingError::Unknown),
         }
     }
 
@@ -29,10 +46,24 @@ impl<'obj, 'de: 'obj> Object<'obj, 'de> {
         }
     }
 
+    pub const fn try_list(self) -> Result<ListDecoder<'obj, 'de>, DecodingError> {
+        match self {
+            Object::List(list_decoder) => Ok(list_decoder),
+            _ => Err(DecodingError::Unknown),
+        }
+    }
+
     pub const fn dictionary(self) -> Option<DictionaryDecoder<'obj, 'de>> {
         match self {
             Object::Dictionary(dictionary_decoder) => Some(dictionary_decoder),
             _ => None,
+        }
+    }
+
+    pub const fn try_dictionary(self) -> Result<DictionaryDecoder<'obj, 'de>, DecodingError> {
+        match self {
+            Object::Dictionary(dictionary_decoder) => Ok(dictionary_decoder),
+            _ => Err(DecodingError::Unknown),
         }
     }
 }
